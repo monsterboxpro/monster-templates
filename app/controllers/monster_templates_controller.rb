549f5dda15cbc7eba04c71950862fa1a719b8f49
Andrew Brown <andrew@monsterboxpro.com>
2014-09-08 14:23:47 -0400

    fix issues with protected script tags

--- app/controllers/monster_templates_controller.rb
+++ app/controllers/monster_templates_controller.rb
@@ -1,4 +1,6 @@
 class MonsterTemplatesController < ApplicationController
+  skip_before_action :verify_authenticity_token
+  skip_before_action :login_required
   def index
     respond_to do |f|
       f.js { render layout: false }
