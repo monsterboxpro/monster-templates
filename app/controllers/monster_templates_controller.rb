86f21d80bfad6741cb99c23b0923fff8d3c969f7
Andrew Brown <andrew@monsterboxpro.com>
2014-09-08 14:38:40 -0400

    another fix

--- app/controllers/monster_templates_controller.rb
+++ app/controllers/monster_templates_controller.rb
@@ -1,6 +1,7 @@
 class MonsterTemplatesController < ApplicationController
   skip_before_action :verify_authenticity_token
-  skip_before_action :login_required
+  skip_before_filter :login_required
+
   def index
     respond_to do |f|
       f.js { render layout: false }
--- lib/monster_templates/version.rb
+++ lib/monster_templates/version.rb
@@ -1,3 +1,3 @@
 module MonsterTemplates
-  VERSION = "0.0.7"
+  VERSION = "0.0.8"
 end
